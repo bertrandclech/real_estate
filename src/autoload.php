321480b2a6dc718b52eb6373bf31a604b5545a63
Gwigzz <jhonguetter199@hotmail.fr>
2022-03-03 18:19:38 +0100

    rename db

--- src/autoload.php
+++ src/autoload.php
@@ -1,6 +1,6 @@
 <?php
 
-// Autoload
+// Autoload : path => "./src/Classes/..."
 spl_autoload_register(function ($className) {
     require_once '.' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Classes' . DIRECTORY_SEPARATOR . $className . '.php';
 });
